a0c44453c7badae3867f8f5617bd6729896d01a9
Arkadiusz Machalica <a.machalica@selleo.com>
2022-03-24 18:20:27 +0100

    Simplify routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Route, NavLink, useLocation } from "react-router-dom";
+import { Routes, Route, NavLink } from "react-router-dom";
 
 import {
   ContextExample,
@@ -10,37 +10,27 @@ import {
 } from "./pages/Basic";
 
 function App() {
-  const { pathname } = useLocation();
-
-  const isDeployed = /^\/react_devpath_examples/.test(pathname);
-  const url = isDeployed ? "react_devpath_examples" : "";
-
   return (
     <div className="App">
       <nav className="nav">
         <h3>Examples</h3>
-        <NavLink to={`${url}/basics/handler`}>
-          Interaction handler definition
-        </NavLink>
-        <NavLink to={`${url}/basics/bigComponentOptimization`}>
+        <NavLink to="/basics/handler">Interaction handler definition</NavLink>
+        <NavLink to="/basics/bigComponentOptimization">
           How to optimize big component
         </NavLink>
-        <NavLink to={`${url}/basics/useEffect`}>useEffect flow</NavLink>
-        <NavLink to={`${url}/basics/contextExample`}>Context example</NavLink>
+        <NavLink to="/basics/useEffect">useEffect flow</NavLink>
+        <NavLink to="/basics/contextExample">Context example</NavLink>
       </nav>
       <div className="example">
         <Routes>
-          <Route path={`${url}/basics/handler`} element={<HandlerMethods />} />
+          <Route path="/basics/handler" element={<HandlerMethods />} />
           <Route
-            path={`${url}/basics/bigComponentOptimization`}
+            path="/basics/bigComponentOptimization"
             element={<OptimizationRouter />}
           />
-          <Route path={`${url}/basics/useEffect`} element={<UseEffectFlow />} />
-          <Route path={`${url}/basics/useEffect2`} element={<UseEffect2 />} />
-          <Route
-            path={`${url}/basics/contextExample`}
-            element={<ContextExample />}
-          />
+          <Route path="/basics/useEffect" element={<UseEffectFlow />} />
+          <Route path="/basics/useEffect2" element={<UseEffect2 />} />
+          <Route path="/basics/contextExample" element={<ContextExample />} />
         </Routes>
       </div>
     </div>
